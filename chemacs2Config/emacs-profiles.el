b652c14a2cecf65be9f6af193172e6a74daf06da
Kwee Tan <kwee@eaglecapsoftware.com>
2023-03-28 11:25:13 -0700

    Initial

---
+++ chemacs2Config/emacs-profiles.el
@@ -0,0 +1,10 @@
+(
+ ("default"   . ((user-emacs-directory . "~/emacsConfig/spacemacs.develop")
+                 (env . (("SPACEMACSDIR" . "~/emacsConfig/spacemacs.d")))))
+ ("spacemacs" . ((user-emacs-directory . "~/emacsConfig/spacemacs.develop")
+                 (env . (("SPACEMACSDIR" . "~/emacsConfig/spacemacs.d")))))
+ ("spacemacs0". ((user-emacs-directory . "~/emacsConfig/spacemacs0.develop")
+                 (env . (("SPACEMACSDIR" . "~/emacsConfig/spacemacs0.d")))))
+ ("emacs" . ((user-emacs-directory . "~/emacsConfig/emacs.d")
+             ))
+ )
---
+++ reviveConfig/eg-empty.el
@@ -0,0 +1,13 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+(1
+ (nil
+  ((truncate-partial-width-windows . 50)
+   (make-backup-files)
+   (version-control)
+   (visible-bell)
+   )
+ )
+ (80 33
+   ((0 0 82 31))
+   ((nil "*spacemacs*" 667 1)))
+)
