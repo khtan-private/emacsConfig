d6491bddd64f51246760efd864828f8561f76b69
Kwee Tan @ rooibos <tan.k.h@juno.com>
2023-03-29 16:07:32 -0700

    cosmetic

--- chemacs2Config/emacs-profiles.el
+++ chemacs2Config/emacs-profiles.el
@@ -5,6 +5,5 @@
                  (env . (("SPACEMACSDIR" . "~/emacsConfig/spacemacs.d")))))
  ("spacemacs0". ((user-emacs-directory . "~/emacsConfig/spacemacs0.develop")
                  (env . (("SPACEMACSDIR" . "~/emacsConfig/spacemacs0.d")))))
- ("emacs" . ((user-emacs-directory . "~/emacsConfig/emacs.d")
-             ))
- )
+ ("emacs" . ((user-emacs-directory . "~/emacsConfig/emacs.d")))
+)
