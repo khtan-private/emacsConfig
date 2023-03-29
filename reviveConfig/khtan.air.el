64919bd2c031d88621c51e2a7edbb0c244c0df3d
Kwee Tan <kwee@eaglecapsoftware.com>
2023-03-28 18:22:12 -0700

    Customize khtan.air.el, play.air.el for //air

---
+++ reviveConfig/khtan.air.el
@@ -0,0 +1,13 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+;; cp emacsConfig/reviveConfig/khtan.air.el .revive.el
+(1
+ ((
+   (nil "S-home" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/")))
+   ("c:/Users/tankh/GDrive/tan.k.h.usa@gmail.com/za/learn.org" "learn.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "c:/Users/tankh/GDrive/tan.k.h.usa@gmail.com/za/")))
+   ("c:/Users/tankh/GDrive/tan.k.h.usa@gmail.com/za/st.org" "st.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "c:/Users/tankh/GDrive/tan.k.h.usa@gmail.com/za/st.org")))
+  )
+ )
+ (80 33
+     ((0 0 82 31))
+     ((nil "S-home" 667 1)))
+)
