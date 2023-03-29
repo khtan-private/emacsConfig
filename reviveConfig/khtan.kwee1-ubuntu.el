ed6eda07e1d97d5bd174512a90b4fce1eed238a7
KHT@kwee1-ubuntu <tan.k.h@juno.com>
2023-03-28 21:41:57 -0700

    reviveConfig/{khtan,play}.kwee1-ubuntu.el added

---
+++ reviveConfig/khtan.kwee1-ubuntu.el
@@ -0,0 +1,12 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+(1
+ ((
+   (nil "S-home" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/")))
+   ("/mnt/g/My Drive/za/learn.org" "learn.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "/mnt/g/My Drive/za/")))
+   ("/mnt/g/My Drive/za/st.org" "st.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "/mnt/g/My Drive/za/")))
+  )
+ )
+ (80 33
+     ((0 0 82 31))
+     ((nil "S-home" 667 1)))
+)
---
+++ reviveConfig/play.kwee1-ubuntu.el
@@ -0,0 +1,13 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+(1
+ ((
+   (nil "S-play" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/cprojects/github/play/")))
+   (nil "S-elisp" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/cprojects/github/play/elisp/")))
+   (nil "S-dw1" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/cprojects/github/play/dw1/")))
+   ("~/cprojects/github/play/notes.org" "notes.org<play>" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "~/cprojects/github/play/")))
+  )
+ )
+ (80 33
+     ((0 0 82 31))
+     ((nil "S-play" 667 1)))
+)
