aa749e4352ed159c5683d6d1147bc6af03528f7f
Kwee Tan @ rooibos <tan.k.h@juno.com>
2023-03-29 18:40:48 -0700

    Able to get khtan and play working on rooibos via online account to google drive

---
+++ reviveConfig/khtan.rooibos.el
@@ -0,0 +1,12 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+(1
+ ((
+   (nil "S-home" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/")))
+   ("/run/user/1000/gvfs/google-drive:host=gmail.com,user=tan.k.h.usa/1lJtRCovGuPvdAljQbvFm8CJsEELCwCVC/11Rpqb0Qb602miQP4WPvYnazC-Luw1XPF" "learn.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "/run/user/1000/gvfs/google-drive:host=gmail.com,user=tan.k.h.usa/1lJtRCovGuPvdAljQbvFm8CJsEELCwCVC")))
+   ("/run/user/1000/gvfs/google-drive:host=gmail.com,user=tan.k.h.usa/1lJtRCovGuPvdAljQbvFm8CJsEELCwCVC/1-64swwYIZp5i2tZE-pnpCsLGtoP_uLoV" "st.org" org-mode 10 100 ((buffer-read-only) (truncate-lines . t) (fill-column . 70) (fill-prefix) (case-fold-search . t) (default-directory . "/run/user/1000/gvfs/google-drive:host=gmail.com,user=tan.k.h.usa/1lJtRCovGuPvdAljQbvFm8CJsEELCwCVC")))
+  )
+ )
+ (80 33
+     ((0 0 82 31))
+     ((nil "S-home" 667 1)))
+)
