ccb833620352cb6f2ec65bf8bd3b7a763b96aec7
Kwee Tan <kwee@eaglecapsoftware.com>
2023-04-21 10:00:47 -0700

    added lotus.kwee1-ubuntu.el

---
+++ reviveConfig/lotus.kwee1-ubuntu.el
@@ -0,0 +1,10 @@
+;;;$Id: revive.el,v 2.19 2008/05/13 01:19:16 yuuji Exp $
+(1
+ ((
+   (nil "S-lotus" shell-mode 0 0 ((buffer-read-only) (truncate-lines) (fill-column . 80) (fill-prefix) (case-fold-search . t)  (default-directory . "~/cprojects/lotus/")))
+  )
+ )
+ (80 33
+     ((0 0 82 31))
+     ((nil "S-lotus" 667 1)))
+)
