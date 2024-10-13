8df10886ef7a3750c92535fdee53a203f78bf700
Kwee Tan <kwee@eaglecapsoftware.com>
2024-10-12 21:58:45 -0700

    Add fonts for I Ching usage

---
+++ emacs.d/init.el
@@ -0,0 +1,5 @@
+  ;; Set primary font
+  (set-face-attribute 'default nil :font "Source Code Pro-10")
+
+  ;; Set fallback font for Unicode characters
+  (set-fontset-font t 'unicode (font-spec :family "DejaVu Sans" :size 32) nil 'append)
