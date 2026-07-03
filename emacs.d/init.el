265f6525d5e98ad1046f9dda7a791c8791f50859
Kwee Tan <kwee@eaglecapsoftware.com>
2026-07-03 16:05:12 -0700

    emacs: use wombat dark theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,8 @@
-  ;; Set primary font
-  (set-face-attribute 'default nil :font "Source Code Pro-10")
+;; Set dark theme
+(load-theme 'wombat t)
 
-  ;; Set fallback font for Unicode characters
-  (set-fontset-font t 'unicode (font-spec :family "DejaVu Sans" :size 32) nil 'append)
+;; Set primary font
+(set-face-attribute 'default nil :font "Source Code Pro-10")
+
+;; Set fallback font for Unicode characters
+(set-fontset-font t 'unicode (font-spec :family "DejaVu Sans" :size 32) nil 'append)
